79f4c40c1d35426449f7bcb5e15f218a2583bfaa
Ankit <ankit28595@gmail.com>
2020-02-21 10:46:36 +0530

    Added infra for messages, frontend required

--- contest/admin.py
+++ contest/admin.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 
-from .models import Question, Hint, Contestant, Answer, Contest
+from .models import Question, Hint, Contestant, Answer, Contest, Message
 
 
 class AnswerInLine(admin.TabularInline):
     model = Answer
-    readonly_fields = ['text', 'question', 'time']
+    readonly_fields = ['question', 'text', 'time']
+    extra = 1
+
+
+class MessageInline(admin.TabularInline):
+    model = Message
+    extra = 0
+    readonly_fields = ['seen']
 
 
 @admin.register(Contestant)
 class MyUserAdmin(admin.ModelAdmin):
-    inlines = [AnswerInLine]
+    inlines = [AnswerInLine, MessageInline]
     list_display = ['username', 'name', 'email', 'college', 'points', 'extra_time']
-    fields = ['username',  'name', 'email', 'college', 'points', 'extra_time', 'answered_questions']
+    fields = ['username', 'name', 'email', 'college', 'points', 'extra_time', 'answered_questions']
     filter_horizontal = ['answered_questions']
     # readonly_fields = ['answered_questions']
     search_fields = ['username', 'first_name', 'last_name', 'email']
